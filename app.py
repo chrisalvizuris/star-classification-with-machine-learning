22f27d4504f45af1f95cfcee40f756870d2bbef0
chris alvizuris <chris.alvizuris@gmail.com>
2021-07-08 22:57:24 -0700

    Added nav bar and created data visualization page

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+
+@app.route('/data-visualization')
+def data_visualization():
+    return render_template("data-visualization.html")
 
 
 if __name__ == '__main__':
